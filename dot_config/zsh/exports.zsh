f4c0c8f6d18df861b5ee6a32e0a0f7e2b1b5214b
Alex Wicks <alex@awicks.io>
2021-12-15 18:03:18 +0000

    Updating zsh exports; adding kubectl alias scripts

--- dot_config/zsh/exports.zsh
+++ dot_config/zsh/exports.zsh
@@ -4,6 +4,7 @@ export GOPATH=$HOME/.local/go
 export EDITOR=nvim
 export QT_QPA_PLATFORMTHEME=qt5ct
 export CLICOLOR=1
+export DOCKER_BUILDKIT=1
 
 export FZF_DEFAULT_OPTS='--height 30% --layout=reverse'
 export STEAM_COMPAT_DATA_PATH=$HOME/.proton
